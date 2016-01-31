72491457c34c24043160b56612f61310195ace9d
Arek Bisaga <ethnar86@gmail.com>
2016-01-31 17:24:13 +0100

    Core functionality
    Main controller and template

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -3,5 +3,41 @@
 angular.module('SampleApp', [])
 
 .controller('SampleAppController', function ($scope) {
-	
+	var controller = this;
+
+	$scope.model = {
+		datasets: []
+	};
+
+	$scope.init = function () {
+		controller.loadDatasets();
+	};
+
+	$scope.addDataset = function () {
+		$scope.model.datasets.push({});
+	};
+
+	$scope.removeDataset = function (index) {
+		$scope.model.datasets.splice(index, 1);
+	};
+
+	$scope.calculateMax = function () {
+		var max = 0;
+		_.each($scope.model.datasets, function (dataset) {
+			max = Math.max(max, controller.calculateValue(dataset));
+		});
+		return max;
+	};
+
+	$scope.outputDatasets = function () {
+		console.log(angular.toJson($scope.model.datasets));
+	};
+
+	controller.calculateValue = function () {
+		// TODO
+	};
+
+	controller.loadDatasets = function () {
+		// TODO
+	};
 });
